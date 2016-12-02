7eba7701085b6f799105d37c860599a3fe92df29
Julian Chung <juliancwt77@gmail.com>
2016-12-02 11:47:16 +0800

    fixed job profile page

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -11,7 +11,15 @@ class CandidatesController < ApplicationController
   end
 
   def show
-    @posting = Candidate.find(params[:id])
+    @candidate = Candidate.find(params[:id])
+
+  end
+
+  def new
+  @new_candidate = Candidate.new
+  unless user_signed_in?
+  redirect_to new_user_session_path, :alert => "Please log in to create job profile"
+  end
   end
 
   def create
@@ -19,14 +27,16 @@ class CandidatesController < ApplicationController
       @new_candidate = Candidate.new()
       @new_candidate.title = params[:candidate][:title]
       @new_candidate.language = params[:candidate][:language]
-      @new_candidate.description = params[:candidate[:description]
+      @new_candidate.description = params[:candidate][:description]
       @new_candidate.rate = params[:candidate][:rate]
       @new_candidate.status = params[:candidate][:status]
       @new_candidate.photo = params[:candidate][:photo]
+      @new_candidate.candidate_id = current_user.id
+      @new_candidate.errors.full_messages
 
       respond_to do |format|
         if @new_candidate.save
-          format.html { redirect_to myproperties_path, notice: 'Your property is successfully created.' }
+          format.html { redirect_to candidate_path id: @new_candidate.id, notice: 'Your profile is successfully created.' }
           format.json { render :show, status: :ok, location: @new_candidate }
         else
           format.html { render :new }
@@ -36,6 +46,12 @@ class CandidatesController < ApplicationController
   end
 
 
+  private
+
+
+  def candidate_params
+  params.require(:candidate).permit(:title,:language, :description, :rate, :status, :photo, :candidate_id)
+  end
 
 
 
