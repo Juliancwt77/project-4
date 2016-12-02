c961f65f35e9e3fac7eb25dd7fb296fba2368801
Braden CT Tan <tct13@yahoo.com>
2016-12-02 13:37:36 +0800

    completed first individual posting page

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -1,11 +1,22 @@
 class PostingsController < ApplicationController
 
   def index
-    @postings = Candidate.where('status > ?', 0).reverse_order
+    # @postings = Candidate.where('status > ?', 0).reverse_order
+    @postings = Posting.all.reverse_order
+
+    respond_to do |format|
+    format.html
+    format.json { render json: @postings }
+    end
   end
 
   def show
-    @posting = Candidate.find(params[:id])
+    @posting = Posting.find(params[:id])
+
+    respond_to do |format|
+    format.html
+    format.json { render json: @posting }
+    end
   end
 
 end
--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,6 +1,6 @@
 class Posting < ApplicationRecord
 
-  belongs_to :candidate
+  belongs_to :cand_info, class_name: 'Candidate', primary_key: 'id'
   has_many :recruits
 
 end
