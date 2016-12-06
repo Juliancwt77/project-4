700451e2499e0d4aa73f0e278b08dd01db84c760
Braden CT Tan <tct13@yahoo.com>
2016-12-06 09:37:19 +0800

    slight changes added for heroku upload

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -10,6 +10,7 @@ class PostingsController < ApplicationController
     end
   end
 
+
   def show
     @posting = Posting.find(params[:id])
 
@@ -19,51 +20,50 @@ class PostingsController < ApplicationController
     end
   end
 
+
   def new
-  @posting = Posting.new
-  @candiates=current_user.candidates
+    @posting = Posting.new
+    @candiates=current_user.candidates
   end
+
 
   def create
 
-  @candidate = Candidate.find(params[:candidate_id])
-  @new_posting = Posting.new()
-  @new_posting.candidate_id = @candidate.id
-  @new_posting.save
-  @update_posting =
-  @candidate.status = 1
-  @candidate.save
-  @new_posting.errors.full_messages
+    @candidate = Candidate.find(params[:candidate_id])
+    @new_posting = Posting.new()
+    @new_posting.candidate_id = @candidate.id
+    @new_posting.save
+
+    @candidate.status = 1
+    @candidate.save
+    @new_posting.errors.full_messages
 
 
-  respond_to do |format|
-    if @new_posting.save
-      format.html { redirect_to candidate_path id: @candidate.id}
-      format.json { render :show, status: :ok, location: @new_posting }
-    else
-      format.html { render :new }
-      format.json { render json: @new_posting.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @new_posting.save
+        format.html { redirect_to candidate_path id: @candidate.id }
+        format.json { render :show, status: :ok, location: @new_posting }
+      else
+        format.html { render :new }
+        format.json { render json: @new_posting.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  end
 
   def destroy
     @candidate = Candidate.find(params[:id])
     @posting= Posting.find(params[:candidate_id])
     # @posting = Posting.find(params[:id])
-  # @posting.candidate_id = @candidate.id
+    # @posting.candidate_id = @candidate.id
 
     @candidate.status = 0
     @candidate.save
     @posting.destroy
     respond_to do |format|
-      format.html { redirect_to candidate_url, notice: 'You have successfully deleted your posting' }
+      format.html { redirect_to candidate_path, notice: 'You have successfully deleted your posting' }
       format.json { head :no_content }
+    end
   end
-
-  end
-
-
 
 end
