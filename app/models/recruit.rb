9bef8c7c925959d38ccf9758291de977fa1f4bbb
Braden CT Tan <tct13@yahoo.com>
2016-12-08 15:53:59 +0800

    minor changes to front page

--- app/models/recruit.rb
+++ app/models/recruit.rb
@@ -3,4 +3,8 @@ class Recruit < ApplicationRecord
   belongs_to :posting
   belongs_to :company, class_name: 'User', primary_key: 'id'
 
+  validates :scope, length: { maximum: 100 }, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
 end
